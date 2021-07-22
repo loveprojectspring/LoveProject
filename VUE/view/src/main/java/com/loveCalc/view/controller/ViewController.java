179d70d2b4fdbdc53af96a2268522deffa4b42e9
elghaied <eslam.elghaied@ohrizon.com>
2021-07-22 12:27:00 +0200

    View with fake data

--- VUE/view/src/main/java/com/loveCalc/view/controller/ViewController.java
+++ VUE/view/src/main/java/com/loveCalc/view/controller/ViewController.java
@@ -1,8 +1,11 @@
 package com.loveCalc.view.controller;
 
+import com.loveCalc.view.dto.Couple;
+import com.loveCalc.view.dto.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,11 +14,22 @@ public class ViewController {
 
     @GetMapping
     public String homePage(Model model) {
+        Couple couple = new Couple();
+        couple.setFirstPerson("sam");
+        couple.setPercentage("69");
+        couple.setSecondPerson("Han");
+        model.addAttribute("couple", couple);
         return "home";
     }
 
     @GetMapping("/couple")
     public String coupleList(Model model) {
         return "couple";
+    }
+
+    @PostMapping("/match")
+    public String makeMatch(Person firstPerson, Person secondPerson){
+        return "home";
+
     }
 }
