945b830da0b115a4a3731585f2104da4a3c0f620
elghaied <eslam.elghaied@ohrizon.com>
2021-07-22 16:34:27 +0200

    Match history page

--- VUE/view/src/main/java/com/loveCalc/view/controller/ViewController.java
+++ VUE/view/src/main/java/com/loveCalc/view/controller/ViewController.java
@@ -9,29 +9,42 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 @Controller
 @RequestMapping("/")
 public class ViewController {
     Couple matched = new Couple();
+    List<Couple> couples = new ArrayList<Couple>();
 
     @GetMapping
     public String homePage(Model model) {
 
-        if(matched.getPercentage() == null){
-            matched.setPercentage("0");
-        }
+
         model.addAttribute("couple", matched);
         return "home";
     }
 
     @GetMapping("/couple")
     public String coupleList(Model model) {
+        model.addAttribute("couples", couples);
         return "couple";
     }
 
     @PostMapping(value = "/match/", consumes = MediaType.ALL_VALUE)
     public String makeMatch(Couple couple){
 
+        Random r = new Random();
+        int low = 10;
+        int high = 100;
+        int result = r.nextInt(high-low) + low;
+
+
+        couple.setPercentage(result);
+
+        couples.add(couple);
         matched = couple;
         System.out.println(couple);
         return "redirect:/";
