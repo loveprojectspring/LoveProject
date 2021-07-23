28e91d85a65d6fad23e74c725a53c0e9e760b3ce
ClementINTI <clement.wasselet@laposte.net>
2021-07-23 15:22:50 +0200

    GAteway fonctionnelle

--- VUE/view/src/main/java/com/loveCalc/view/controller/CallApiExterneController.java
+++ VUE/view/src/main/java/com/loveCalc/view/controller/CallApiExterneController.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 
 public class CallApiExterneController {
 
+	private static String uriServiceAPI ="http://localhost:8200";
+	
     public static Couple callServiceApi(String name1, String name2) throws IOException {
         /** Instance du service de la request **/
         RequestService requestService = new RequestService();
@@ -16,7 +18,7 @@ public class CallApiExterneController {
         /** Test **/
 //        requestService.addServiceApi("http://localhost:8600/response/Michel/Claudine");
         /** Dynamic **/
-        requestService.addServiceApi("http://localhost:8600/response/"+name1+"/"+name2);
+        requestService.addServiceApi(uriServiceAPI+"/response/"+name1+"/"+name2);
 
         /** Service result **/
         CoupleApi rsp = requestService.getResponse();
