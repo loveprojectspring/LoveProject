1b9fa0c65ef30f5f6b724e874930619fc52daf2a
Nicolas Rondet <rondet.nicolas@gmail.com>
2021-07-23 11:00:29 +0200

    JPP please

--- BDD/DB/love/src/main/java/com/audrey/love/controller/CoupleController.java
+++ BDD/DB/love/src/main/java/com/audrey/love/controller/CoupleController.java
@@ -15,8 +15,21 @@ public class LoverController {
 
     list<Couple> couple = new ArrayList<Couple>();
 
-    public void add(Couple couple){
+    couple.add('Clément','Clément',99,'Le centipède')
+    couple.add('Mickey','Jef Bezos',12,'La magie de disney n\'opère pas ')
+    couple.add('Luke','Anakin',99,'Tant que ca reste en famille')
+    couple.add('RIRI','FIFI',99,'OUAH')
+    couple.add('LOULOU','POUTOU',2,'LOVE')
+    couple.add('Lama','Scarabet',19,'L\'amour est dans le prés')
+    couple.add("Chien","Chat",0,'Les poilus')
+    couple.add('Eslam','Hugo',20000,'Spring power')
+    couple.add('Mathieu','Gateway',99,'Il faut passer par là')
+
+/*   wtf
+
+    public void getLover(Couple couple){
         couple.add(couple);
-    }
+    }*/
+
 
 }
